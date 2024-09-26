56e852db6db140625095f5f533dcc03eda122a47
kadench <kadench@byui.edu>
2024-09-26 17:18:53 -0600

    Updated files for more consistency
    The table cell background color has been renamed to gray instead of blue because it's more of a grayish color. Also, web manifest was causing an error. That has been fixed.

--- format-cells.js
+++ format-cells.js
@@ -3,7 +3,6 @@ const lastTableRow = tableRows[tableRows.length - 1];
 const lastRightP = lastTableRow.querySelector(".right-div p")
 const lastLeftP = lastTableRow.querySelector(".left-div p")
 
-
 // Make sure the bottom-most table row has the correct bottom border applied.
 lastRightP.classList.add("bottom-cell");
 lastLeftP.classList.add("bottom-cell");
@@ -19,23 +18,32 @@ for (let i = tableRows.length - 1; i >= 0; i--) {
     }
 
 }
-// Add "blue" as a prefix to status classes for remaining rows
+// Add "gray" as a prefix to status classes for remaining rows
 tableRows.forEach((row) => {
     // Select all <p> elements inside the row
     row.querySelectorAll(".left-div p, .right-div p").forEach((p) => {
-        if (!p.classList.contains("blue")) {
-            p.classList.add("blue")
+
+        // If the current cell doesn't have gray as a class.
+        if (!p.classList.contains("gray")) {
+
+            // Add it.
+            p.classList.add("gray");
+        };
+        
+        // if any p that has a gray background, update the class to use the gray foreground color
+        let classCollection = p.classList;
+
+        if (classCollection.contains("progress")) {
+            classCollection.replace("progress", "gray-progress");
+            
+        } else if (classCollection.contains("help")) {
+            classCollection.replace("help", "gray-help");
+            
+        } else if (classCollection.contains("miss")) {
+            classCollection.replace("miss", "gray-miss");
+            
+        } else if (classCollection.contains("complete")) {
+            classCollection.replace("complete", "gray-complete");
         }
-        p.classList.forEach((className) => {
-            if (className.endsWith("progress")) {
-                p.classList.replace(className, "blue-progress");
-            } else if (className.endsWith("help")) {
-                p.classList.replace(className, "blue-help");
-            } else if (className.endsWith("miss")) {
-                p.classList.replace(className, "blue-miss");
-            } else if (className.endsWith("complete")) {
-                p.classList.replace(className, "blue-complete");
-            }
-        });
     });
 });
