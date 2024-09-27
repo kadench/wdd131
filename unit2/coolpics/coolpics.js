96613a9981373f49eaa96d16bd0aff9cca49845d
kadench <kadench@byui.edu>
2024-09-27 12:39:59 -0600

    Finished final activity Unit 2

--- unit2/coolpics/coolpics.js
+++ unit2/coolpics/coolpics.js
@@ -2,16 +2,11 @@
 const menuButton = document.querySelector(".subtitle");
 const mainNav = document.querySelector(".nav-container");
 
-// image gallery variables
-const imgs = Array.from(document.querySelectorAll("figure img"));
-const changeImgScreenSize = window.matchMedia("(min-width: 1024px)");
+// Image gallery modal functionality
+const galleryImages = Array.from(document.querySelectorAll(".gallery-image"));
 
 // When the menu is on screen, look for click.
 menuButton.addEventListener("click", ToggleMenu);
-
-// When viewport changes size: "(min-width: 1024px)", switch the
-// gallery images to the full one to avoid blur and for fun.
-changeImgScreenSize.addEventListener("change", ChangeImage);
 
 // function called to give and remove hiding the ul nav when menu is clicked.
 function ToggleMenu() {
@@ -19,44 +14,47 @@ function ToggleMenu() {
     mainNav.classList.toggle("hidden");
 }
 
-// function that changes all images to the larger image when "(min-width: 1024px)" is met.
-function ChangeImage() {
-    if (changeImgScreenSize.matches) {
-        imgs.forEach((img) => {
-            img.src = "images/norris-full.jpeg";
-        })
-    }
-};
+// Give each image an Event Listener
+galleryImages.forEach((image) => {
+    image.addEventListener("click", ViewHandler)
+})
 
-// Image gallery modal functionality
-const modalExitButton = document.querySelector(".close-viewer");
-
-// When "X" is clicked "close" the modal by hiding it, until an image in gallery is clicked.
-modalExitButton.addEventListener("click", ExitModal);
 
 function ExitModal() {
     const viewerDiv = document.querySelector(".viewer");
     viewerDiv.classList.toggle("hidden")
 }
 
-function viewerTemplate(pic, alt) {
+function ViewerTemplate(pic, alt="full landscape image") {
     return `<div class="viewer">
-      <button class="close-viewer">X</button>
-      <img src="${pic}" alt="${alt}">
-      </div>`;
-  }
+    <button class="close-viewer">X</button>
+    <img src="${pic}" alt="${alt}">
+    </div>`;
+}
 
-
-  function viewHandler(event) {
-	// create a variable to hold the element that was clicked on from event.target
-
+function ViewHandler(event) {
+    // create a variable to hold the element that was clicked on from event.target
+    const clickedImage = event.target;
+    
 	// get the src attribute from that element and 'split' it on the "-"
-
+    let imageSrc = String(clickedImage.src);
+    const imageSrcArray = imageSrc.split("-");
+    
+    if (imageSrcArray[1] === "sm.jpeg") {
+        imageSrcArray[1] = "full.jpeg";
+    }
+    imageSrc = imageSrcArray.join("-");
+    
 	// construct the new image file name by adding "-full.jpeg" to the first part of the array from the previous step
-
+    const modalInformation = ViewerTemplate(imageSrc);
+    
 	// insert the viewerTemplate into the top of the body element
 	// (element.insertAdjacentHTML("afterbegin", htmltoinsert))
-
+    document.body.insertAdjacentHTML("afterbegin", modalInformation);
+    
 	// add a listener to the close button (X) that calls a function called closeViewer when clicked
+    // When "X" is clicked "close" the modal by hiding it, until an image in gallery is clicked.
+    modalExitButton = document.querySelector(".close-viewer")
+    modalExitButton.addEventListener("click", ExitModal);
 
 }
