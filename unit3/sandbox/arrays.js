5dd108d61663ed94c0416fdbffdc1c117e677851
kadench <144969637+kadench@users.noreply.github.com>
2024-10-03 10:01:15 -0600

    Class Thursday done

--- unit3/sandbox/arrays.js
+++ unit3/sandbox/arrays.js
@@ -8,3 +8,4 @@ const listTemplate = (step) => {
 
 const stepsHtml = steps.map(listTemplate); // use map to convert the list from strings to HTML
 document.querySelector("#myList").innerHTML = stepsHtml.join("") // set the innerHTML
+
---
+++ unit3/sandbox/notesr.js
@@ -0,0 +1,22 @@
+// functions are hoisted to the top of the page when code is compiling,
+// but function expressions do not hoist up. Best practice is to define
+// functions first, then call them. "Code Stink"
+
+// Stop. Organize thoughts. Take a big problem by defining it in steps.
+// If you can get it down in steps, then you can start thinking of code
+
+// GPA Calculator
+// 1. Build a user interface
+// 2. gotta have a way to get input from the user
+// 3. Calculate and manipulate information based on input
+// 4. Display the result
+
+// This is a bit too condensed. How can we make this more of a plan
+// than a general list.
+
+// GPA Calculator
+// Build a concise and understandable UI
+// Ask the user for their input
+// remove any white space and any other unneeded character from user input
+// Make all letters UPPERCASE
+// Pass it into the function
